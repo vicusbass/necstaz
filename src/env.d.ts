745512421f4fb412c3c2e4dcfe1e18a0e6ddf943
Vasile Pop <vasile.pop@gmail.com>
2026-01-15 22:15:07 +0200

    added supabase order saving

---
+++ src/env.d.ts
@@ -0,0 +1,15 @@
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly SUPABASE_URL: string;
+  readonly SUPABASE_SERVICE_ROLE_KEY: string;
+  readonly RESEND_API_KEY: string;
+  readonly VERCEL_SITE_URL: string;
+  readonly NETOPIA_API_KEY?: string;
+  readonly NETOPIA_POS_SIGNATURE?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
